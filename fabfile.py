49e737766ad67d7c2b05461358fcda862f893561
Dustin Farris <dustin@dustinfarris.com>
2013-01-22 18:19:48 -0500

    Brackets

--- fabfile.py
+++ fabfile.py
@@ -5,8 +5,8 @@ env.project_name = '{{ project_name }}'
 env.django_settings_module = '{{ project_name }}.settings'
 env.django_test_settings_module = '{{ project_name }}.settings.test'
 # env.roledefs = {
-#   'staging': 'web@12.12.12.12',
-#   'production': 'web@45.45.45.45'}
+#   'staging': ['web@12.12.12.12'],
+#   'production': ['web@45.45.45.45']}
 
 
 from automation import *
