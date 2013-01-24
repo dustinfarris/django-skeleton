c6af232a61932986ea0b5fb1fda60b5fa1043f94
Dustin Farris <dustin@dustinfarris.com>
2013-01-23 19:38:10 -0500

    Added repo_source config to fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ from fabric.api import env
 
 
 env.project_name = '{{ project_name }}'
+env.repo_source = 'git@github.com:yournamehere/{{ project_name }}.git'
 env.django_settings_module = '{{ project_name }}.settings'
 env.django_test_settings_module = '{{ project_name }}.settings.test'
 # env.roledefs = {
